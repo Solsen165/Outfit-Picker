02d2c1ccb1c29ce2862ecac4cb34a3e0cbbbf58c
Jihad Garti <jihad.garti2@gmail.com>
2023-12-18 15:30:41 +0800

    Button Added

--- app/src/main/java/com/example/outfitpicker/MainActivity.kt
+++ app/src/main/java/com/example/outfitpicker/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.outfitpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 : Button = findViewById(R.id.button)
+
+        btn1.setOnClickListener(){
+            Toast.makeText(this, "Jihad is nice", Toast.LENGTH_SHORT).show()
+        }
     }
 }
