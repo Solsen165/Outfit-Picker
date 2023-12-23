db7023d7931b473e80fa1b721778b489c91a9325
Jihad Garti <jihad.garti2@gmail.com>
2023-12-23 11:24:25 +0800

    i'm done with the dialog and the camera stuff still need to add the second view which contains the picture and the attributes

--- app/src/main/java/com/example/outfitpicker/MainActivity.kt
+++ app/src/main/java/com/example/outfitpicker/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -18,23 +19,28 @@ class MainActivity : AppCompatActivity() {
         dialog.setContentView(R.layout.dialog_layout)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        val tvMessage : TextView = dialog.findViewById(R.id.select_txt)
-        val fromgallerybutton : Button = dialog.findViewById(R.id.from_gallery_btn)
-        val cambtn : Button = dialog.findViewById(R.id.camera_btn)
-        val cancelbtn : Button = dialog.findViewById(R.id.cancel_btn)
+        // Set the layout parameters
+        val window = dialog.window
+        window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
 
-        cambtn.setOnClickListener{
-            Toast.makeText(this, "camera opened",Toast.LENGTH_SHORT).show()
+        val tvMessage: TextView = dialog.findViewById(R.id.select_txt)
+        val fromgallerybutton: Button = dialog.findViewById(R.id.from_gallery_btn)
+        val cambtn: Button = dialog.findViewById(R.id.camera_btn)
+        val cancelbtn: Button = dialog.findViewById(R.id.cancel_btn)
+
+        cambtn.setOnClickListener {
+            Toast.makeText(this, "camera opened", Toast.LENGTH_SHORT).show()
         }
 
-        fromgallerybutton.setOnClickListener{
-            Toast.makeText(this, "gallery opened",Toast.LENGTH_SHORT).show()
+        fromgallerybutton.setOnClickListener {
+            Toast.makeText(this, "gallery opened", Toast.LENGTH_SHORT).show()
         }
-        cancelbtn.setOnClickListener{
+        cancelbtn.setOnClickListener {
             dialog.dismiss()
         }
         dialog.show()
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
