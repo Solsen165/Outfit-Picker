96a85294ebf98a2cce40927bb9f9b6d92cf1f5e9
solsen165 <islammohsen165@gmail.com>
2023-12-23 13:23:24 +0800

    Removed the camera permissions

--- app/src/main/java/com/example/outfitpicker/Outfit.kt
+++ app/src/main/java/com/example/outfitpicker/Outfit.kt
@@ -5,9 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity("outfit_table")
 data class Outfit(
-    val name : String,
-    val items : List<Item>
-){
     @PrimaryKey(autoGenerate = true)
-    val id : Int = 0
-}
+    val id : Int = 0,
+    val name : String,
+    //val items : List<Item>
+)
